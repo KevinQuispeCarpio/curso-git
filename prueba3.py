ce2f2b78f99b6d8b98c9b6129f0722adc072c73f
Kevin Quispe <Kevin.quispe.27@unsch.edu.pe>
2023-01-27 15:40:57 -0500

    Son otros ejemplos de commit

--- prueba3.py
+++ prueba3.py
@@ -1,4 +1,6 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 print([1])
-print(x.append([69]))
+print(x.append(69))
 print(x)
+y = x.insert(1,12)
+print(y)
--- suerte.py
+++ suerte.py
@@ -3,10 +3,11 @@ def funncion (x):
     if x == 10:
         print('Estas en o correcto')
     else:
-        funncion(x)
         print('Esa no es mi edad :(')
 
 x = input('Adivina mi edad: ')
 
 print(funncion(x))
-    
+
+# sEGUIMOS INTENTANDO HACER ESTA RAMA, Pero no se que
+#mas hacer 
